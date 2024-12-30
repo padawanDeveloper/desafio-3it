b0ca6ec140b542a6c2e4e67ff3a93f265e9375fd
esteban ortiz <ematiasortiz@gmail.com>
2024-12-30 00:37:45 -0300

    add mock screen

--- src/screens/IndicatorDetail/index.tsx
+++ src/screens/IndicatorDetail/index.tsx
@@ -1,11 +1,55 @@
-import { View, Dimensions } from 'react-native';
+import { ScrollView, View } from 'react-native';
+import { Text } from '@rneui/themed';
+
 import LineChartComponent from './components/charts/Line';
+
+const Header = () => (
+  <>
+    <View>
+      <Text
+        style={{
+          textAlign: 'center',
+        }}
+        h1>
+        $ 913,43
+      </Text>
+    </View>
+    <View
+      style={{
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        marginHorizontal: 20,
+      }}>
+      <Text>Nombre</Text>
+      <Text>euro</Text>
+    </View>
+    <View
+      style={{
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        marginHorizontal: 20,
+      }}>
+      <Text>Nombre</Text>
+      <Text>euro</Text>
+    </View>
+    <View
+      style={{
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        marginHorizontal: 20,
+      }}>
+      <Text>Nombre</Text>
+      <Text>euro</Text>
+    </View>
+  </>
+);
 
 const IndicatorDetail = () => {
   return (
-    <View>
+    <ScrollView>
+      <Header />
       <LineChartComponent />
-    </View>
+    </ScrollView>
   );
 };
 
